ccc7bd80ab68ddb08f08e100e18be4af63c4d564
KConlon13 <KyleConlon13@gmail.com>
2019-10-17 17:17:18 -0400

    added counter buttons to page, no functionality just yet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
   return (
     <div className="App">
       <h1>Kyle's Redux Counter App</h1>
+      <h1>{counter}</h1>
+      
+      <button class="button">+</button>
+      <button class="button">-</button>
+
+      <div>
+        {isLogged ? <h3>Hidden Spooky Information 👻</h3> : null}
+      </div>
     </div>
   );
 }
