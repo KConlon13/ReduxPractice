9f0ef46ed27a77b96480b57d1802b75569783a84
KConlon13 <KyleConlon13@gmail.com>
2019-10-25 19:17:10 -0400

    working on fetching data using redux

--- src/Container.js
+++ src/Container.js
@@ -5,14 +5,27 @@ import { restaurants } from './actions'
 function Container () {
     const counter = useSelector(state => state.counter);
     const restaurantsArray = useSelector(state => state.restaurants)
+    const restNames = []
 
-    // fetch()
+
+    fetch("https://chewsy-backend-api.herokuapp.com/restaurants")
+    .then(response=>response.json())
+    .then(data => 
+        // restaurantsArray.push(data),
+        // console.log(data.map(rest => rest.name))
+        restNames.push(data.map(rest => rest.name)),
+        console.log(restNames)
+        ) 
+
+    let restys = restNames.map(rest=> <h1>{rest.name}</h1>)
 
     return (
         <div>
             <h1>Hello this is the Container</h1>
             <h3>And this is the Current Counter</h3>
             {counter}
+            {/* {restNames === [] ? null : <div>{restNames}</div>} */}
+            {restys}
         </div>
     )
 }
