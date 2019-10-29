c8560bf557a37c2d09ba5a24016e2311026b8f49
KConlon13 <KyleConlon13@gmail.com>
2019-10-28 21:16:34 -0400

    made changes to restaurant state

--- src/Container.js
+++ src/Container.js
@@ -11,11 +11,9 @@ function Container () {
     fetch("https://chewsy-backend-api.herokuapp.com/restaurants")
     .then(response=>response.json())
     .then(data => 
-        // restaurantsArray.push(data),
-        // console.log(data.map(rest => rest.name))
         restNames.push(data.map(rest => rest.name)),
-        console.log(restNames)
-        ) 
+        console.log(restaurantsArray)
+    ) 
 
     let restys = restNames.map(rest=> <h1>{rest.name}</h1>)
 
@@ -25,7 +23,7 @@ function Container () {
             <h3>And this is the Current Counter</h3>
             {counter}
             {/* {restNames === [] ? null : <div>{restNames}</div>} */}
-            {restys}
+            {restaurantsArray}
         </div>
     )
 }
