3b80282d834a34cf62b87ddd866cc4625389bf86
KConlon13 <KyleConlon13@gmail.com>
2019-11-07 16:27:19 -0500

    added routes to App for different pages

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import Nav from "./Nav";
 import About from "./About";
 import Shop from "./Shop";
 import Home from "./Home"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Nav/>
-      <Home/>
-      <About/>
-      <Shop/>
-    </div>
+    <Router>
+      <div>
+        <Nav/>
+        <Route path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/shop" component={Shop}/>
+      </div>
+    </Router>
   );
 }
 
