e3a401374c82d45d0294e384df442c27014b1fc7
KConlon13 <KyleConlon13@gmail.com>
2019-11-08 15:17:03 -0500

    added links for restaurant names to begin dynamic routing

--- src/Shop.js
+++ src/Shop.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Card from "./Card"
+import { Link } from 'react-router-dom';
 
 function Shop(){
 
@@ -17,13 +18,15 @@ function Shop(){
         setItems(items)
     };
 
-    const cardComponents = items.map(item => 
-    <Card key={item.id} obj={item} restArray={items}/> )
-
     return (
         <div>
-            <h1>Shop Page Here!</h1>
-             { cardComponents } 
+            <h1>Shop Page</h1>
+            
+            {items.map(item => 
+                <h3 key={item.restaurant_id} obj={item}>
+                    <Link to={`/shop/${item.restaurant_id}`}>{item.name}</Link>
+                </h3>
+            )}
         </div>
     )
 };
